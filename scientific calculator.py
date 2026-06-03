2a9dbf74204953c613a1365e04d0dc6f0deff63a
mahmud6p <232311216@vu.edu.bd>
2026-06-03 22:15:16 +0600

    Commit 1: Initial setup for scientific calculator

---
+++ scientific calculator.py
@@ -0,0 +1,11 @@
+# Scientific Calculator Project
+# Author: Prince Mahmud
+
+def main():
+    print("Welcome to Scientific Calculator")
+    print("Choose an operation:")
+    print("1. Addition")
+    print("2. Subtraction")
+
+if __name__ == "__main__":
+    main()
