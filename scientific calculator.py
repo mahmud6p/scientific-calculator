ce1d1b740efbf424718f50f23feb6ce27b8c4858
mahmud6p <232311216@vu.edu.bd>
2026-06-09 21:04:55 +0600

    Commit 3: Placed all buttons inside centered frame

--- scientific calculator.py
+++ scientific calculator.py
@@ -224,3 +224,15 @@ main_frame.place(relx=0.5, rely=0.5, anchor="center")
 entry = tk.Entry(main_frame, width=20, font=("Consolas", 18),
                  bg="black", fg="lime", insertbackground="white")
 entry.grid(row=0, column=0, columnspan=4, pady=10)
+
+buttons = [
+    ('7',1,0), ('8',1,1), ('9',1,2), ('+',1,3),
+    ('4',2,0), ('5',2,1), ('6',2,2), ('-',2,3),
+    ('1',3,0), ('2',3,1), ('3',3,2), ('*',3,3),
+    ('0',4,0), ('.',4,1), ('=',4,2), ('/',4,3),
+]
+
+for (text,row,col) in buttons:
+    tk.Button(main_frame, text=text, width=5, height=2,
+              bg="purple", fg="white",
+              command=lambda t=text: entry.insert(tk.END, t)).grid(row=row, column=col, padx=5, pady=5)
